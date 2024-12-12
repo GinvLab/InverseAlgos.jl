290261182e73832f3403cc68c7f332bb79a6a61c
Andrea Zunino <inverseproblem@users.noreply.github.com>
2024-12-12 18:05:55 +0100

    suppress log messages in L-BFGS tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,31 +4,42 @@ using Test
 using LinearAlgebra
 using InverseAlgos.Optimizers
 using InverseAlgos.KronLinInv
-
+using Logging
 
 include("utils.jl")
 include("test_lmbfgs.jl")
 include("test_kronlininv.jl")
 
 
-# Optimizers
-@testset "Test L-BFGS algo" begin
-
-    @test test_bfgs1()
-    @test test_bfgs2()
-
-end
 
 
-# KronLinInv
-@testset "Test KronLinInv" begin
-
-    printstyled("Testing 2D example \n", bold=true,color=:cyan)
-    @test test2D()
-
-    printstyled("Testing 3D example \n", bold=true,color=:cyan)
-    @test test3D()
+# suppress all messages from algos
+logger = NullLogger()
+with_logger(logger) do
 
     println()
-end
 
+    # Optimizers
+    @testset "Test L-BFGS algo" begin
+
+        printstyled("L-BFGS: Test 1\n", bold=true,color=:cyan)
+        @test test_bfgs1()
+        printstyled("L-BFGS: Test 2\n", bold=true,color=:cyan)
+        @test test_bfgs2()
+
+    end
+
+
+    # KronLinInv
+    @testset "Test KronLinInv" begin
+
+        printstyled("KronLinInv: Testing 2D example \n", bold=true,color=:cyan)
+        @test test2D()
+
+        printstyled("KronLinInv: Testing 3D example \n", bold=true,color=:cyan)
+        @test test3D()
+
+        println()
+    end
+
+end
